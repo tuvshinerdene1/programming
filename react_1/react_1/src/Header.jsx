b43ee10abce183646e8adb90c00886b393112fdb
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-11-16 19:40:33 +0800

    finished

---
+++ react_1/react_1/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+
+            </nav>
+        </header>
+    );
+}
+export default Header
