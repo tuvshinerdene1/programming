07ae30e301e4964db81db1acf6deee667a49a10a
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-11-18 00:26:27 +0800

    finished surface

---
+++ django_1/django_1/news/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("articles/<int:year>/", views.year_archive),
+    path("article/<int:year>/<int:month>/",views.month_archive),
+path("articles/<int:year>/<int:month>/<int:pk>/", views.article_detail),]
