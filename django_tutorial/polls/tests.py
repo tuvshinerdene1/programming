0cdd376d4bfc06503fd71c177656ba55568b26b5
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-11-24 15:03:53 +0800

    wrote unit testing

--- django_tutorial/polls/tests.py
+++ django_tutorial/polls/tests.py
@@ -1,3 +1,20 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        futureQuestion = Question(pub_date = time)
+        self.assertIs(futureQuestion.was_published_recently(), False)
+
+    def test_was_published_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        oldQuestion = Question(pub_date = time)
+        self.assertIs(oldQuestion.was_published_recently(),False)
+
+    def test_was_published_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recentQuestion = Question(pub_date = time)
+        self.assertIs(recentQuestion.was_published_recently(), True)
