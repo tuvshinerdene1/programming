23f3af5874f4ac91edbb5d95660d5a6a3712b592
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-11-24 22:07:37 +0800

    this

--- django_tutorial/polls/tests.py
+++ django_tutorial/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 from .models import Question
+from django.urls import reverse
 
 class QuestionModelTests(TestCase):
     def test_was_published_with_future_question(self):
@@ -18,3 +19,12 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recentQuestion = Question(pub_date = time)
         self.assertIs(recentQuestion.was_published_recently(), True)
+def create_question(question_text, days):
+    time = timezone.now()+datetime.timedelta(days=days)
+    return Question.objects.create(question_text = question_text, pub_date = time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
